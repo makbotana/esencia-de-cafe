9db4daae58bb3a133e04a9ae8584a4961ff8e9c8
araguzzo <araguzzo@gmail.com>
2022-10-22 22:10:39 -0300

    formulario validado

--- javascript/formulario.js
+++ javascript/formulario.js
@@ -1,3 +1,51 @@
-const nombre = document.getElementById("name")
-const email = document.getElementById("email")
-const numero = document.getElementById("number")
+function enviarFormularioContacto(){
+    const nombreElement = document.getElementById("name")
+    const emailElement = document.getElementById("email")
+    const telefonoElement = document.getElementById("tel")
+    const mensajeElement = document.getElementById("message")
+
+    const data = {
+        nombre:nombreElement.value,
+        email: emailElement.value, 
+        telefono: telefonoElement.value,
+        mensaje: mensajeElement.value, 
+    }
+
+    if(data.nombre === ""){
+        window.alert("Se require un nombre");
+        return null;
+    }
+    if(data.email === ""){
+        window.alert("Se requiere un email");
+        return null;
+    }
+    if(data.telefono === ""){
+        window.alert("Se requiere un número");
+        return null;
+    }
+    if(data.mensaje === ""){
+        window.alert("Se requiere un mensaje");
+        return null;
+    }
+
+    if(
+        !data.email.includes("@") || 
+        !data.email.endsWith(".com") ||
+        !data.email.endsWith(".org") ||
+        !data.email.endsWith(".edu") ||
+        !data.email.endsWith(".ar") ||
+        !data.email.endsWith(".gov") 
+
+    ){
+        window.alert("No es un email válido");
+        return null;
+    }
+
+    if(isNaN(Number(data.telefono))){
+        window.alert("Teléfono no es un número válido");
+        return null;
+    }
+    
+    console.log(`Enviando formulario: ${JSON.stringify(data, null, 2)}`)
+    
+}
